81f2234339a198fe744745885ae70067f5661313
classsikh <pc2june@gmail.com>
2024-07-12 17:31:24 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base:"/BasketballPlayerCard/",
   plugins: [react()],
+  base: "/card_assignment",
   server: {
     port: 3000,
   }
